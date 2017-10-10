9262cab480279a94717b02769dba7d6c717b7d6f
Nathan Merris <joorge.jetson@gmail.com>
2017-10-10 16:26:47 -0400

    initial commit

---
+++ src/com/nmerris/Main.java
@@ -0,0 +1,35 @@
+package com.nmerris;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BufferedReader reader = null;
+
+        try {
+            File file = new File("simple.bmp");
+            reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    } // main
+}
