643e27464eeb4a3049c98af8438763a6bfd3822c
Nathan Merris <joorge.jetson@gmail.com>
2017-10-10 17:46:23 -0400

    reading in sample.bmp and correctly extracting the width and height from the byte array

--- src/com/nmerris/Main.java
+++ src/com/nmerris/Main.java
@@ -1,35 +1,43 @@
 package com.nmerris;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+// author: Nathan Merris
 public class Main {
 
     public static void main(String[] args) {
 
-        BufferedReader reader = null;
+        Scanner scanner = new Scanner(System.in);
+
+        // get the file path from the user
+        // example file path: /home/nmerris/IdeaProjects/bitmap header reader/src/com/nmerris/simple.bmp
+        System.out.println("Enter a file path: ");
+        Path filePath = Paths.get(scanner.nextLine());
 
         try {
-            File file = new File("simple.bmp");
-            reader = new BufferedReader(new FileReader(file));
+            // convert the whole file into an array of bytes
+            byte[] data = Files.readAllBytes(filePath);
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+            // wrap it in a little endian byte buffer to make it easier to get at specific byte chunks
+            // NOTE: numbers are stored in little-endian format in bitmap headers, so least significant digit is first
+            ByteBuffer bb = ByteBuffer.wrap(data);
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+
+            // get the width and height: width is 4 bytes, always starts at 18, height always starts at 22
+            int width = bb.getInt(18);
+            int height = bb.getInt(22);
+
+            System.out.println("width: " + width + ", height: " + height);
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-
 
     } // main
 }
